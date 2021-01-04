ad24726634c3a84f0da2c3b579131dae6694b276
JunHo <u_innovation@naver.com>
2021-01-04 19:56:09 +0900

    제대로된 값을 입력할 때까지 반복문 돌리기로 코드 수정

--- 20210104_연산자_제어문_문제풀이/src/문제16.java
+++ 20210104_연산자_제어문_문제풀이/src/문제16.java
@@ -6,19 +6,24 @@ public class 문제16 {
 		Scanner scan = new Scanner(System.in);
 		int a, b, c;
 		System.out.println("=====메뉴=====");
-		System.out.print("김치찌개 - 5000원? ");
-		a = scan.nextInt();
-		System.out.print("제육볶음 - 6000원? ");
-		b = scan.nextInt();
-		System.out.print("된장찌개 - 4000원? ");
-		c = scan.nextInt();
+		while (true) {
 
-		if (a + b + c == 5) {
-			int res = (a * 5000) + (b * 6000) + (c * 4000);
-			int d = res / 5;
-			System.out.println("1인당 계산금액: " + d + "원입니다.");
-		} else {
-			System.out.println("잘못된 주문입니다.");
+			System.out.print("김치찌개 - 5000원? ");
+			a = scan.nextInt();
+			System.out.print("제육볶음 - 6000원? ");
+			b = scan.nextInt();
+			System.out.print("된장찌개 - 4000원? ");
+			c = scan.nextInt();
+
+			if (a + b + c == 5) {
+				int res = (a * 5000) + (b * 6000) + (c * 4000);
+				int d = res / 5;
+				System.out.println("1인당 계산금액: " + d + "원입니다.");
+				break;
+			} else {
+				System.out.println("잘못된 주문입니다.");
+				continue;
+			}
 		}
 	}
 
