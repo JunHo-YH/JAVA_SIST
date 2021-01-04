78109dd8bb360dc724887aab244a208438f6799d
JunHo <u_innovation@naver.com>
2021-01-04 20:04:07 +0900

    조건을 만족할 때까지 반복하는 while문 추가

--- 20210104_연산자_제어문_문제풀이/src/문제22.java
+++ 20210104_연산자_제어문_문제풀이/src/문제22.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class 문제22 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("영어를 입력해보세요: ");
-		String s = scan.next();
-		int count = 0;
 
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (c == 'a' || c == 'A') {
-				count++;
+		while (true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.print("영어를 입력해보세요: ");
+			String s = scan.next();
+			int count = 0;
+
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if (c == 'a' || c == 'A') {
+					count++;
+				}
+			}
+			if (count == 0) {
+				System.out.println("a나 A가 포함되는 문자가 없습니다. 다시 입력하세요");
+				continue;
+			} else {
+				System.out.println("a나 A의 총 개수는: " + count + "개입니다.");
 			}
 		}
-		System.out.println("a나 A의 총 개수는: " + count + "개입니다.");
 	}
 
 }
