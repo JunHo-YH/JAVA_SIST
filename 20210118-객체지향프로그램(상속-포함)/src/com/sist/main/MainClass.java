a1293a6f3e9126e3f78f2a0ea9fc275df32e6035
JunHo <u_innovation@naver.com>
2021-01-18 09:16:57 +0900

    20210118객체지향프로그램(상속,포함)

---
+++ 20210118-객체지향프로그램(상속-포함)/src/com/sist/main/MainClass.java
@@ -0,0 +1,29 @@
+package com.sist.main;
+/*
+ * 	객체지향 프로그램: 유지보수가 가능하게 만드는 프로그램
+ *                =======
+ *                1) 데이터 보호: 캡슐화
+ *                   => 멤버변수는 private으로 은닉화
+ *                   => 접근 => getter / setter
+ *                   => 멤버변수는 감추고 메서드를 통해서 접근하는 방식
+ *                   멤버변수: private
+ *                   멤버메서드: public
+ *                   생성자: 모든 클래스에서 접속이 가능: public
+ *                   ** 상속이 있는 경우에는 멤버변수는 protected
+ *                      === 상속이 가능 (public, protected, default)
+ *                      === 라이브러리를 변경
+ *                2) 기존의 클래스를 재사용: is-a(상속), has-a(포함):웹에서는 포함관계를 주로 사용한다.
+ *                3) 변경, 추가
+ * 1. 캡슐화
+ */
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
