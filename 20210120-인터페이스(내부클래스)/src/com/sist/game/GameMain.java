2ad5299824a7f5c373b1a8bb054bf040c9a65a43
JunHo <u_innovation@naver.com>
2021-01-20 12:37:50 +0900

    2021-01-20 자바로 게임 만들기

--- 20210120-인터페이스(내부클래스)/src/com/sist/game/GameMain.java
+++ 20210120-인터페이스(내부클래스)/src/com/sist/game/GameMain.java
@@ -1,105 +1,120 @@
 package com.sist.game;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-public class GameMain extends JFrame implements KeyListener, MouseListener {
-	GameView gv = new GameView(); // 포함클래스 (있는 그대로 사용)
-
-	public GameMain() {
-		// 윈도우 위에 그림을 올린다 => add
-		add("Center", gv);
-
-		// 윈도우 크기 결정
-		setSize(1024, 768);
-		setVisible(true);
-		gv.addMouseListener(this);
-		gv.addKeyListener(this);
-		gv.setFocusable(true);
-	}
-
+import java.awt.event.*;
+/*
+ *     class ===> class
+ *          extends 
+ *     interface ===> interface
+ *          extends
+ *     interface ===> class
+ *          implements 
+ *     class ===== interface ==== class  
+ *          extends       implements 
+ *     다중 구현 
+ *     interface A
+ *     interface B
+ *     class C implements A,B
+ *     
+ *     interface A
+ *     interface B
+ *     class C
+ *     class D extends C implements A,B
+ *     
+ *     ==> 클래스가 여러개 
+ *         기능이 동일 ===> 수행 과정이 틀릴 경우 => 인터페이스 
+ *         프로그램 맞게 구현해서 사용 
+ *         인터페이스 : default => 필요할 경우에 오버라이딩 => 구현된 내용 사용
+ *     
+ *     
+ */
+public class GameMain extends JFrame implements KeyListener,MouseListener{
+    GameView gv=new GameView();// 포함 클래스 (있는 그대로 사용) => has-a
+    public GameMain()
+    {
+    	// 윈도우 위에 그림을 올린다 => add
+    	add("Center",gv);
+    	// 윈도우 크기 결정
+    	setSize(1024, 768);
+    	// 윈도우를 보여라
+    	setVisible(true);
+    	gv.addMouseListener(this);
+    	gv.addKeyListener(this);
+    	gv.setFocusable(true);
+    	
+    }
 	public static void main(String[] args) {
-		GameMain am = new GameMain();
-
+		// TODO Auto-generated method stub
+        GameMain am=new GameMain();
 	}
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		gv.x = e.getX();
-		gv.y = e.getY();
-		gv.repaint(); // paint()호출
+		// TODO Auto-generated method stub
+		gv.x=e.getX();
+		gv.y=e.getY();
+		gv.repaint();// paint()호출 
 	}
-
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-		gv.setImage();
-		switch (e.getKeyCode()) {
+		// TODO Auto-generated method stub
+		
+		switch(e.getKeyCode())
+		{
 		case KeyEvent.VK_UP:
-			gv.start();
-			gv.y -= 5;
-			if (gv.y < 0) gv.y = 768;
-			gv.repaint();
-			break;
+			 gv.start();
+			 gv.y-=5;
+			 if(gv.y<0) gv.y=768;
+			 gv.repaint();
+			 break;
 		case KeyEvent.VK_DOWN:
-			gv.start();
-			gv.y += 5;
-			if (gv.y > 768) gv.y = 0;
-			gv.repaint();
-			break;
+			 gv.start();// run() 
+			 gv.y+=5;
+			 if(gv.y>768) gv.y=0;
+			 gv.repaint();
+			 break;
 		case KeyEvent.VK_LEFT:
-			gv.start();
-			gv.x -= 5;
-			if(gv.x < 0) gv.x = 1024;
-			gv.repaint();
-			break;
+			 gv.start();
+			 gv.x-=5;
+			 if(gv.x<0) gv.x=1024;
+			 gv.repaint();
+			 break;
 		case KeyEvent.VK_RIGHT:
-			gv.start();
-			gv.x += 5;
-			if(gv.x > 1024) gv.x = 0;
-			gv.repaint();
-			break;
-
+			  gv.start();
+			 gv.x+=5;
+			 if(gv.x>1024) gv.x=0;
+			 gv.repaint();
+			 break;
 		}
 	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- 20210120-인터페이스(내부클래스)/src/com/sist/game/GameView.java
+++ 20210120-인터페이스(내부클래스)/src/com/sist/game/GameView.java
@@ -1,62 +1,57 @@
 package com.sist.game;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-
 /*
- * class ===> class
- *        extends
- * interface ==> interface
- *      extends
- * interface ===> class
- *        implements
- * class =====> interface ===> class
- *       extends           implements
- *      
+ *    /  => //
+ *    "  => /"
+ *    
+ *    JFrame 
+ *    JPanel
+ *    JWindow
+ *    JDialog
  */
-public class GameView extends JPanel {
-	Image back, avata;
-	// 초기값 => 구현 : 초기화 블럭, 생성자
-	int x = 100, y = 300;
-
-	public GameView() {
-		back = Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\background.jpg"); // static 메서드
-		avata = Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_00.png");
-	}
-
-	public void setImage() {
-		for (int i = 0; i < 9; i++) {
-			avata = Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_0" + i + ".png");
-			repaint();
-			try {
-				Thread.sleep(10);
-			} catch(Exception ex) {
-				repaint();
-				
-			}
-		}
-	}
-
+public class GameView extends JPanel{
+    Image back,avata;
+    // 초기값 => 구현 : 초기화 블럭 , 생성자
+    int x=100,y=300;
+    public GameView()
+    {
+    	back=Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\back.jpg");
+    	avata=Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_00.png");
+    }
+    public void setImage()
+    {
+    	for(int i=0;i<9;i++)
+    	{
+    		avata=Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_0"+i+".png");
+    		
+    		try
+    		{
+    			Thread.sleep(100);
+    		}catch(Exception e){}
+    		repaint();
+    	}
+    }
 	@Override
 	public void paint(Graphics g) {
-		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
-		g.drawImage(avata, x, y, this);
-
+		g.drawImage(back,0,0,getWidth(),getHeight(),this);
+		g.drawImage(avata,x,y,this);
 	}
-	public void start() {
-		AvataMove a = new AvataMove();
-		a.start();
-	}
-
-	// 윈도우 그림을 그린다
-	class AvataMove extends Thread {
-		public void run() {
+    public void start()
+    {
+    	AvataMove a=new AvataMove();
+    	a.start();
+    }
+    // 윈도우 그림을 그린다 
+	class AvataMove extends Thread
+	{
+		public void run()
+		{
 			setImage();
-			avata = Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_00.png");
+			avata=Toolkit.getDefaultToolkit().getImage("c:\\javaDev\\player\\my_00.png");
 			repaint();
 		}
-	
 	}
-
+    
+    
 }
