24f97636fb98dd1f37dd06c3b1105129ff8670f6
JunHo <u_innovation@naver.com>
2021-01-20 12:36:43 +0900

    2021-01-20 자바로 게임 만들기

---
+++ 20210120-인터페이스(내부클래스)/src/com/sist/main/MainClass.java
@@ -0,0 +1,57 @@
+package com.sist.main;
+
+/*
+ * 
+ */
+abstract class A {
+	public abstract void aaa();
+
+	public abstract void bbb();
+
+	public abstract void ccc();
+
+	public abstract void ddd();
+
+	public abstract void eee();
+}
+
+class B extends A {
+
+	@Override
+	public void aaa() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void bbb() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ccc() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ddd() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eee() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	}
+
+}
