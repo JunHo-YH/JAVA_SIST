e621a5df7501ebf6f1c7f359775e8ceeade4b23b
JunHo-YH <u_innovation@naver.com>
2021-02-14 23:04:08 +0900

    Logic Test

---
+++ 20210209-데이터베이스(데이터추가)/src/com/sist/movie/Test.java
@@ -0,0 +1,12 @@
+package com.sist.movie;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int i = 65;
+		char c = (char)i;
+		System.out.println(String.valueOf(c));
+	
+	}
+
+}
