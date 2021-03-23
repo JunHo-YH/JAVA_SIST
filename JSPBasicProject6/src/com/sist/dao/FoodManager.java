67b8ea41260adee2d885fe5677237b5bd49c7801
JunHo <u_innovation@naver.com>
2021-03-23 14:19:59 +0900

    인식이 안되면, projec => clean

--- JSPBasicProject6/src/com/sist/dao/FoodManager.java
+++ JSPBasicProject6/src/com/sist/dao/FoodManager.java
@@ -1,5 +1,29 @@
 package com.sist.dao;
 
-public class FoodManager {
+import org.jsoup.Jsoup;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class FoodManager {
+	public void foodCategoryData() {
+		try {
+			int k =1;
+			// 연결 => 소스 읽기
+			Document doc = Jsoup.connect("https://www.mangoplate.com/").get();
+			System.out.println(doc);
+			
+			
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		FoodManager fm = new FoodManager();
+		fm.foodCategoryData();
+	}
+	
 }
--- JSPLastProject/src/com/sist/dao/MainClass.java
+++ JSPLastProject/src/com/sist/dao/MainClass.java
@@ -1,5 +1,8 @@
 package com.sist.dao;
 
 public class MainClass {
-  
+	public static void main(String[] args) {
+		String bb = "abc";
+		System.out.println(bb.charAt(1));
+	}
 }
