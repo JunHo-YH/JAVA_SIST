8a017bb498f7afd302efb3ecbb234886a7c03917
JunHo <u_innovation@naver.com>
2021-04-14 21:37:56 +0900

    Spring DI Project, Mybatis & Annotaion

---
+++ SpringDIProject5/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ SpringDIProject5/src/main/java/com/sist/main/StudentVO.java
@@ -0,0 +1,39 @@
+package com.sist.main;
+
+public class StudentVO {
+	private String name;
+	private int kor, eng, math;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+}
